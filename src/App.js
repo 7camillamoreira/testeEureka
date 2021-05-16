8499f7e66daa53a1e098a61897236aa521da6bdb
CamillaMoreira <camillasmoreira@outlook.com>
2021-05-16 19:52:03 -0300

    primeira lista ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useEffect } from "react";
 
-function App() {
+export default function App() {
+  const [personagens, setPersonagens] = useState([])
+
+  useEffect(async () => {
+    const response = await fetch('https://rickandmortyapi.com/api/character')
+    const data = await response.json();
+
+    setPersonagens(data.results);
+  }, []);
+
+  // useEffect(() => {
+  //   const filtered = personagens.filter(personagem => personagem.favorite);
+
+  //   setPersonagens(data);
+  // }, [];
+
+  function handleFavorite(id) {
+    const newPersonagens = personagens.map(personagem => {
+      return personagem.id === id ? { ...personagem, favorite: !personagem.favorite } : personagem    
+    })
+    setPersonagens(newPersonagens);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ul>
+      {personagens.map(personagem => (
+        <li key={personagem.id}>
+          <img src={personagem.image} />
+          <h3>{personagem.name}</h3>
+          {personagem.favorite && <span>favorito</span>}
+          <button onClick={() => handleFavorite(personagem.id)}>favoritar</button>
+        </li>
+      ))}
+    </ul>
+    );
 }
-
-export default App;
