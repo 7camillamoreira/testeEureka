0a73fc2bc6f7f69c08675c525292cf5fba0ec8f0
CamillaMoreira <camillasmoreira@outlook.com>
2021-05-17 00:39:56 -0300

    segunda parte do teste

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import './App.css';
+import Imagem from "./rickandmorty.png"
 
 export default function App() {
   const [personagens, setPersonagens] = useState([])
+  const [favoritos, setFavoritos] = useState([])
 
   useEffect(async () => {
     const response = await fetch('https://rickandmortyapi.com/api/character')
@@ -11,29 +13,47 @@ export default function App() {
     setPersonagens(data.results);
   }, []);
 
-  // useEffect(() => {
-  //   const filtered = personagens.filter(personagem => personagem.favorite);
-
-  //   setPersonagens(data);
-  // }, [];
+  useEffect(() => {
+    setFavoritos(personagens.filter((personagem) => personagem.favorite))
+  }, [personagens])
 
   function handleFavorite(id) {
     const newPersonagens = personagens.map(personagem => {
-      return personagem.id === id ? { ...personagem, favorite: !personagem.favorite } : personagem    
+      return personagem.id === id ? { ...personagem, favorite: true } : personagem    
+    })
+    setPersonagens(newPersonagens);
+  }
+
+  function handleRemoveFavorite(id) {
+    const newPersonagens = personagens.map(personagem => {
+      return personagem.id === id ? { ...personagem, favorite: false } : personagem    
     })
     setPersonagens(newPersonagens);
   }
 
   return (
-    <ul>
-      {personagens.map(personagem => (
-        <li key={personagem.id}>
-          <img src={personagem.image} />
-          <h3>{personagem.name}</h3>
-          {personagem.favorite && <span>favorito</span>}
-          <button onClick={() => handleFavorite(personagem.id)}>favoritar</button>
-        </li>
-      ))}
-    </ul>
-    );
+    <>
+      <img src={Imagem} width="100%" height="auto" />
+      <div className="divisoria">
+        <ul>
+          {personagens.map(personagem => (
+            <li key={personagem.id}>
+              <img src={personagem.image} alt="foto de um personagem da sereie Rick and Morty"/>
+              <h3>{personagem.name}</h3>
+              <button onClick={() => handleFavorite(personagem.id)}>{personagem.favorite ? "Favorito":"favoritar"}</button>
+            </li>
+          ))}
+        </ul>
+        <ul>
+          {favoritos.map(favorito => (
+            <li key={favorito.id + 'favorito'}>
+              <img src={favorito.image} />
+              <h3>{favorito.name}</h3>
+              <button onClick={() => handleRemoveFavorite(favorito.id)}>Remover</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
